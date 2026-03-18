14a6f0bbc18a2260b46b41a91833e01932d5d9ea
Musab Adam <musabadam95@gmail.com>
2026-03-18 22:07:50 +0000

    Refactor environment variable handling and token logic

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ required_env_vars = [
     'MQTT_PORT',
     'MQTT_USER',
     'MQTT_PASSWORD',
-    'MASJID_SURL'
+    'MASJID_SURL',
+    'POLL_INTERVAL'
 ]
 missing_vars = [var for var in required_env_vars if not os.getenv(var)]
 if missing_vars:
@@ -22,34 +23,33 @@ MQTT_PORT = int(os.getenv('MQTT_PORT'))
 MQTT_USER = os.getenv('MQTT_USER')
 MQTT_PASSWORD = os.getenv('MQTT_PASSWORD')
 MASJID_SURL = os.getenv('MASJID_SURL')
-
-
 MQTT_TOPIC = 'elivemasjid/status'
 MQTT_CLIENT_ID = 'elivemasjid'
-
 STATUS_URL = f"https://emasjidlive.co.uk/listen/{MASJID_SURL}"
 RELAY_URL = f"https://relay.emasjidlive.uk/{MASJID_SURL}?"
 POLL_INTERVAL = int(os.getenv('POLL_INTERVAL'))
 regex_pattern = r"token=(?P<token>[^&]+)&expires=(?P<expires>\d+)"
+last_token = None
+
 class LiveMasjid:
 
     def __init__(self, masjid_surl):
         self.masjid_surl = masjid_surl
         
-    def extract_and_cast(self,rawSource):
-        global last_token
-        match = re.search(regex_pattern,rawSource )
+    def extract_and_cast(self, rawSource):
+        match = re.search(regex_pattern, rawSource )
         if match:
             current_token = match.group('token')
-            current_expires= match.group('expires')
+            current_expires = match.group('expires')
             if current_token != last_token:
                 print(f"New token detected: {current_token}. Updating stream...")
                 print(f"New expiry {current_expires}. ")
                 # 3. Prepare the Home Assistant API Call
                 last_token = current_token # Update our tracker
-                return raw_source_url+f"?token={current_token}&expires={current_expires}"                
+                return RELAY_URL + f"?token={current_token}&expires={current_expires}"                
             else:
                 print("Token hasn't changed. Skipping update.")
+                return RELAY_URL + f"?token={last_token}&expires={current_expires}"                
         else:
             print("Could not find a valid token in the source string.")
     def get_stream_status(self):
@@ -64,7 +64,6 @@ class LiveMasjid:
                 return False
             elif response.status_code == 200:
                 print("Stream Currently Online")
-                
                 return self.extract_and_cast(responseText)
             else:
                 print("Unable to determine status")
